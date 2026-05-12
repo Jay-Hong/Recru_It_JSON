8a59d15d3b663c00b67dd455dc5336f345a5b63e
Jay <hjpyooo@gmail.com>
2026-05-12 10:33:41 +0000

    Recru_It

--- recru_it/recru_it/pipelines.py
+++ recru_it/recru_it/pipelines.py
@@ -78,7 +78,10 @@ class Recru_It_Pipeline:
             raise DropItem('\n\nDrop phone : 010-3287-1198 🚯\n')
         
         elif len(re.compile('010-7558-3993').findall(item['phone'])) > 0: # tem
-            raise DropItem('\n\nDrop phone : 010-3287-1198 🚯\n')
+            raise DropItem('\n\nDrop phone : 010-7558-3993 🚯\n')
+        
+        elif len(re.compile('010-2757-2978').findall(item['phone'])) > 0: # tem
+            raise DropItem('\n\nDrop phone : 010-2757-2978 🚯\n')
         
         # 매산시스템 (경기도 광주인데 자꾸 서울에 올림)
         elif len(re.compile('010-8089-0413').findall(item['phone'])) > 0: # tem
